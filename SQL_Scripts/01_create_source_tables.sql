70f30979fdf85d6017899321f8eaa163a745acb0
Vani Verma <116835677+vani3598@users.noreply.github.com>
2026-05-11 20:57:25 -0400

    Update 01_create_source_tables.sql

--- SQL_Scripts/01_create_source_tables.sql
+++ SQL_Scripts/01_create_source_tables.sql
@@ -5,6 +5,11 @@ CREATE TABLE customers (
     city VARCHAR(50)
 );
 
+CREATE TABLE product_category (
+    category_id INT,
+    category_name VARCHAR(100)
+);
+
 CREATE TABLE products (
     product_id INT,
     product_name VARCHAR(100),
@@ -12,9 +17,18 @@ CREATE TABLE products (
     price DECIMAL(10,2)
 );
 
+CREATE TABLE orders (
+    order_id INT,
+    customer_id INT,
+    order_date DATE,
+    order_status VARCHAR(50)
+);
+
 CREATE TABLE sales (
     sale_id INT,
+    order_id INT,
     product_id INT,
     quantity INT,
-    total_amount DECIMAL(10,2)
+    total_amount DECIMAL(10,2),
+    sale_date DATE
 );
