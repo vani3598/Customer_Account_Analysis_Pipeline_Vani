9bf4c17639476b856c8d447cd487c835d6b251ad
Vani Verma <116835677+vani3598@users.noreply.github.com>
2026-05-11 20:58:49 -0400

    Create 04_validation_queries.sql

---
+++ SQL_Scripts/04_validation_queries.sql
@@ -0,0 +1,13 @@
+SELECT * FROM customers;
+SELECT * FROM product_category;
+SELECT * FROM products;
+SELECT * FROM orders;
+SELECT * FROM sales;
+
+SELECT COUNT(*) AS customer_count FROM customers;
+SELECT COUNT(*) AS product_count FROM products;
+SELECT COUNT(*) AS sales_count FROM sales;
+
+SELECT * FROM sales_summary_gold;
+SELECT * FROM category_sales_summary_gold;
+SELECT * FROM top_products_gold;
